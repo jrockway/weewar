33cf913e824652a3fa9adcc08b671d4e12de4edc
Jonathan Rockway <jon@jrock.us>
2007-09-16 21:51:26 -0500

    flesh out Weewar.pm docs; add method to get Weewar::HQ objects

--- lib/Weewar.pm
+++ lib/Weewar.pm
@@ -18,7 +18,7 @@ Readonly my $base   => $ENV{WEEWAR_BASE} || 'api1';
 
 =head1 NAME
 
-Weewar - interact with the C<weewar.com> API
+Weewar - get data from the weewar.com XML API
 
 =head1 SYNOPSIS
 
@@ -51,13 +51,14 @@ what data you can get from the API.
 =head1 METHODS
 
 Right now, everything is a class method since the weewar API is public
-for everything except the HQ.  If this changes, then this API will
-change a bit.
+for everything except the HQ (and no state needs to be kept between
+requests).  If this changes, then this API will change a bit.
 
 =cut
 
+# separate method so that WeewarTest can override the HTTP part
 sub _get {
-    my ($class, $path) = @_;
+    my ($class, $path, $args) = @_;
     
     my $ua = LWP::UserAgent->new;
     my $res = $ua->get("http://$server/$base/$path");
@@ -67,8 +68,8 @@ sub _get {
 }
 
 sub _request {
-    my ($class, $path) = @_;
-    my $content = $class->_get($path);
+    my ($class, $path, $args) = @_;
+    my $content = $class->_get($path, $args);
     my $parser = XML::LibXML->new;
     return $parser->parse_string($content);
 }
@@ -76,9 +77,11 @@ sub _request {
 =head all_users
 
 Return a list of all active Weewar users as L<Weewar::User> objects.
-The objects are loaded lazily, so this method only cause
+The objects are loaded lazily, so this method only causes one request
+to be sent to the server.  When you start accessing the returned
+children, they will be populated on-demand from the server.
 
-sub all_users
+An exception will be thrown if something goes wrong.
 
 =cut
 
@@ -97,6 +100,13 @@ sub all_users {
     return @users;
 }
 
+=head2 user($username)
+
+Returns a C<Weewar::User> object representing C<$username>.  If there is
+no user by that name, and exception is thrown.
+
+=cut
+
 sub user {
     my $class     = shift;
     my $username  = shift;
@@ -104,6 +114,13 @@ sub user {
     $user->draws; # force the object to be populated
     return $user;
 }
+
+=head2 game($id)
+
+Returns a C<Weewar::Game> object representing the game with id C<$id>.  If 
+there is no game with that id, an exception is thrown.
+
+=cut
 
 sub game {
     my $class   = shift;
@@ -113,11 +130,61 @@ sub game {
     return $game;   
 }
 
+=head2 hq($username => $apikey)
+
+Returns a C<Weewar::HQ> object representing C<$username>'s
+"headquarters".  If there is an error getting the data (bad API key,
+etc.), an exception is thrown.
+
+=cut
+
 sub hq {
     my $class = shift;
     my ($user, $key) = @_;
     my $hq = Weewar::HQ->new({ key => $key, user => $user });
     return $hq;
 }
+
+=head1 ENVIRONMENT
+
+You can use different weewar servers by changing these environment
+variables.  I doubt there are other weewar servers that speak this
+API, though.
+
+=over 4
+
+=item WEEWAR_SERVER
+
+The hostname of the Weewar server, defaulting to C<weewar.com>
+
+=item WEEWAR_BASE
+
+The base URL of the API, defaulting to C<api1>.
+
+=back
+
+=head1 BUGS
+
+If the Weewar API changes, this module will need an update.  Let me
+know if something is broken so I can fix it.
+
+The combination of Weewar's odd XML, C<XML::LibXML>, and the fact that
+I had very little sleep before writing this makes for some very ugly
+code.  Feel free to clean it up and send me a patch.
+
+Bugs should be reported through RT, but you can email me directly too.
+
+=head1 AUTHOR
+
+Jonathan Rockway C<< <jrockway@cpan.org> >>
+
+=head1 COPYRIGHT
+
+This module is copyright (c) 2007 Jonathan Rockway.
+
+You can distribute, modify, and use this module under the same terms
+as Perl itself.
+
+=cut
 
 1;
